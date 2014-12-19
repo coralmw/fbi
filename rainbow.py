4c93448e26c2fb5f2a376c0897b3db09086c2c59
Thomas Parks <thomasparks@outlook.com>
2014-12-19 00:01:39 +0000

    color shifting works

--- rainbow.py
+++ rainbow.py
@@ -10,3 +10,39 @@ black = np.zeros((1440, 900))
 
 #reasonably fast
 fb[:] = black
+
+def color(r, g, b):
+    #16 bit has 5 bit r, b and 6-bit g
+    #5+6 == 11
+    assert(r < 32)
+    assert(b < 32)
+    assert(g < 64)
+    return r << 11 | g << 5 | b
+
+def create_sprial(size=(1440, 900)):
+    array = np.zeros(size) 
+    for n in range(100):
+        x = int(n*np.cos(n))
+        y = int(n*np.sin(n))
+        array[x, y] = 2**16
+    return array
+
+sprial = np.zeros((1440, 900), dtype='uint16') 
+for n in range(100):
+    x = int(n*np.cos(n))
+    y = int(n*np.sin(n))
+    x += 1440/2
+    y += 900/2
+    sprial[x, y] = 2**16-1
+
+
+sprial = np.zeros((1440, 900), dtype='uint16') 
+for x in np.linspa:
+    for y in range(900):
+        if int(np.sqrt(x**2 + y**2)) == 100:
+            sprial[x, y] = 2**16-1
+
+fb[:] = 
+
+def color(r, g, b):
+    return r << 11 | g << 5 | b
